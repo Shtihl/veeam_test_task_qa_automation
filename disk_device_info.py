a165c9f424b12219f166b0a44e8f6b111ce14386
Dmitriy Firsov <Shtihl@localhost.localdomain>
2020-03-03 22:12:48 +0300

    complete task1

--- disk_device_info.py
+++ disk_device_info.py
@@ -2,41 +2,39 @@ import sys
 from subprocess import run, PIPE
 
 
-def output_info(drive_details):
-    return drive_details
-
-
-def parse_drive_info(drive_info):
-    drive_details = list(filter(None, drive_info.split(' ')))
-    if drive_details[1] == 'disk':
-        return drive_details
-    else:
-        additional_info = run(
-            f'df -h -T {drive_details[0]}',
-            shell=True,
-            stdout=PIPE,
-            encoding='utf-8'
-        ).stdout.rstrip()
-        return drive_details.extend(additional_info)
-
-
-def get_drive_info(drive):
-    drive_info = run(
+def command_lsblk(drive):
+    command = run(
         f'lsblk {drive} -d -p -l -n -o name,type,size',
         shell=True,
         stdout=PIPE,
         encoding='utf-8'
-    ).stdout.rstrip()
-    if 'disk' in drive_info:
-        return drive_info
-    else:
-        additional_info = run(
-            f'df -h -T {drive[0]}',
-            shell=True,
-            stdout=PIPE,
-            encoding='utf-8'
-        ).stdout.rstrip()
-        return drive_info + additional_info
+    ).stdout.rstrip().split()
+    return command
+
+
+def command_df(drive):
+    command = run(
+        f'df -h -T {drive}',
+        shell=True,
+        stdout=PIPE,
+        encoding='utf-8'
+    ).stdout.rstrip().split('\n')[1].split()
+    return command
+
+
+def get_drive_info(drive):
+    lsblk_out = command_lsblk(drive)
+    drive_info = {
+        'address': lsblk_out[0],
+        'type': lsblk_out[1],
+        'size': lsblk_out[2]
+    }
+    if drive_info['type'] != 'disk':
+        df_out = command_df(drive)
+        drive_info['avail'] = df_out[4]
+        drive_info['fstype'] = df_out[1]
+        drive_info['mount'] = df_out[6]
+    return drive_info
 
 
 def load_drive_address(filepath):
@@ -50,6 +48,7 @@ def load_drive_address(filepath):
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         drive_address = load_drive_address(sys.argv[1])
-        print(get_drive_info(drive_address))
+        device_info = get_drive_info(drive_address)
+        print(' '.join(device_info.values()))
     else:
         print('Usage: python driveinfo.py <file_name.txt>')
